3c7a4fe6cf71eece7090f547cdb59b4534bdaf37
dcfcgadders <dcfcgadders@gmail.com>
2017-06-08 13:38:59 +0100

    Create index.php
    This is the html structure for building the postcode calculation form

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <title>Distance Calculator</title>
+  </head>
+  <body>
+    <div class="postcode_section">
+      <p><input id="deliveryfrom" type="text" size="8" placeholder="START POSTCODE"></p>
+      <p><input id="deliveryto" type="text" size="8" placeholder="END POSTCODE"></p>
+      <br/>
+      <br/>
+      <select id="van_type">
+        <option id="select_small" value="small">Small Van</option>
+        <option id="select_large" value="large">Large Van</option>
+        <option id="select_truck" value="truck">7.5t Truck</option>
+      </select>
+      <br/>
+      <br/>
+      <button id="get_prices">Get Prices</button>
+    </div>
+    <div class="results_section">
+      <div id="from"></div>
+      <div id="to"></div>
+      <div id="duration"></div>
+      <div id="dist_miles"></div>
+      <div id="dist_cost"></div>
+      <p>&nbsp;</p>
+      <div id="map"></div>
+      <p>&nbsp;</p>
+      <button id="reset">reset</button>
+    </div>
+  </body>
+</html>
